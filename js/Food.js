a356ad82ea8e6c27cd254c45154880bb7eb1a83b
PrishaMadane <70759166+PrishaMadane@users.noreply.github.com>
2022-02-23 18:35:52 +0530

    Update Food.js

--- js/Food.js
+++ js/Food.js
@@ -1,41 +1,43 @@
-class Food{
+class Food {
     constructor(){
-        this.image=loadImage("../images/Milk.png");
-        this.foodStock
-        this.lastFed
+    this.foodStock=0;
+    this.lastFed;
+    this.image=loadImage('Images/Milk.png');
+    }
+
+   updateFoodStock(foodStock){
+    this.foodStock=foodStock;
+   }
+
+   getFedTime(lastFed){
+     this.lastFed=lastFed;
+   }
+
+   deductFood(){
+     if(this.foodStock>0){
+      this.foodStock=this.foodStock-1;
+     }
     }
 
     getFoodStock(){
-        return this.foodStock
-    }
-    updateFoodStock(foodStock){
-        this.foodStock = foodStock;
-    }
-    deductFood(){
-        if (this.foodStock > 0){
-            this.foodStock = this.foodStock-1
-        }
-    }
-
-    getFeedTime(lastFed){
-        this.lastFed = lastFed
+      return this.foodStock;
     }
 
     display(){
       var x=80,y=100;
-      console.log(this.foodStock);
-      imageMode(CENTER)
+      
+      imageMode(CENTER);
       image(this.image,720,220,70,70);
-
+      
       if(this.foodStock!=0){
-          for(var i=0;i<this.foodStock;i++){
-              if(i%10==0){
-                  x=80;
-                  y=y+50
-              }
-              image(this.image,x,y,50,50);
-              x=x+30
+        for(var i=0;i<this.foodStock;i++){
+          if(i%10==0){
+            x=80;
+            y=y+50;
           }
+          image(this.image,x,y,50,50);
+          x=x+30;
+        }
       }
-      }
+    }
 }
